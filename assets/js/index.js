f0b295bb83655dde81a08bc953c3464354f1c598
Boris Arco <anton.arck@gmail.com>
2022-03-13 18:58:40 -0300

    Agregando funcionalidades

---
+++ assets/js/index.js
@@ -0,0 +1,30 @@
+
+// Smooth Scroll
+$(function() {
+  $("a").click(function (event) {
+    if (this.hash !=="") {
+        event.preventDefault();
+        var gato = this.hash;
+        $("html, body").animate({
+            scrollTop: $(gato).offset().top
+        }, 1400);
+    }
+
+  });
+});
+$(document).scroll(function(e){
+  const y = $("html").scrollTop();
+  if (y > 300) $("nav").addClass("nav-black")
+  else $("nav").removeClass("nav-black")
+})
+
+// Alert boton enviar
+$("#boton-enviar").click(function(){
+    alert ("el correo fue enviado");
+    });
+
+// Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
